188f84b54b6a699f1d9c0623a1499bc5f0474799
Simon Brunning <simon.brunning3@nhs.net>
2025-03-10 14:21:09 +0000

    Upload coverage from unit tests, use that for coverage report.

--- scripts/tests/coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd "$(git rev-parse --show-toplevel)"
-
-make dependencies install-python
-poetry run pytest tests/unit/ --durations=10 --cov-report= --cov src/eligibility_signposting_api
-poetry run python -m coverage xml
