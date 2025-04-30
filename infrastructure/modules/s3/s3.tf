10ebd7b19718b1498355e14011736d3ed078b967
TOEL2 <tom.eldridge1@nhs.net>
2025-04-30 15:29:49 +0100

    Changes by ash

--- infrastructure/modules/s3/s3.tf
+++ infrastructure/modules/s3/s3.tf
@@ -1,1 +1,143 @@
 # s3
+# Define bucket
+resource "aws_s3_bucket" "storage_bucket" {
+  bucket = "${var.project_name}-${var.environment}-${var.bucket_name}"
+}
+
+# Enable versioning for disaster recovery
+resource "aws_s3_bucket_versioning" "storage_bucket_versioning_config" {
+  bucket = aws_s3_bucket.storage_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Block public access to the bucket
+resource "aws_s3_bucket_public_access_block" "storage_bucket_block_public_access" {
+  bucket = aws_s3_bucket.storage_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Encrypt the bucket with a KMS key
+resource "aws_s3_bucket_server_side_encryption_configuration" "storage_bucket_server_side_encryption_config" {
+  bucket = aws_s3_bucket.storage_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.storage_bucket_cmk.arn
+      sse_algorithm     = "aws:kms"
+    }
+    bucket_key_enabled = true
+  }
+}
+
+#Lifecycle config
+resource "aws_s3_bucket_lifecycle_configuration" "storage_bucket" {
+  bucket = aws_s3_bucket.storage_bucket.id
+
+  rule {
+    id     = "StorageBucketExpirationTransferToIa"
+    status = "Enabled"
+    filter {
+      prefix = ""
+    }
+
+    expiration {
+      days = "${var.bucket_expiration_days}"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "STANDARD_IA"
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+}
+
+#same again for logging buckets
+resource "aws_s3_bucket" "storage_bucket_access_logs" {
+  bucket = "${var.project_name}-${var.environment}-${var.bucket_name}-access-logs"
+}
+
+resource "aws_s3_bucket_logging" "storage_bucket_logging_config" {
+  bucket        = aws_s3_bucket.storage_bucket.id
+  target_bucket = aws_s3_bucket.storage_bucket_access_logs.bucket
+  target_prefix = "bucket_logs/"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "storage_bucket_access_logs_server_side_encryption_config" {
+  bucket = aws_s3_bucket.storage_bucket_access_logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "storage_bucket_access_logs_object_expiry_lifecycle_rule_config" {
+  bucket = aws_s3_bucket.storage_bucket_access_logs.id
+
+  rule {
+    id     = "StorageBucketLogsExpiration"
+    status = "Enabled"
+    filter {
+      prefix = ""
+    }
+    expiration {
+      days = var.log_retention_in_days
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = var.log_retention_in_days
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "storage_bucket_access_logs_public_access_block" {
+  bucket = aws_s3_bucket.storage_bucket_access_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "storage_bucket_access_logs_policy" {
+  bucket = aws_s3_bucket.storage_bucket_access_logs.id
+  policy = data.aws_iam_policy_document.storage_bucket_access_logs_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "storage_bucket_access_logs_policy" {
+  statement {
+    sid = "AllowSSLRequestsOnly"
+    actions = [
+      "s3:*",
+    ]
+    effect = "Deny"
+    resources = [
+      aws_s3_bucket.storage_bucket_access_logs.arn,
+      "${aws_s3_bucket.storage_bucket_access_logs.arn}/*",
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test = "Bool"
+      values = [
+        "false",
+      ]
+
+      variable = "aws:SecureTransport"
+    }
+  }
+}
+
