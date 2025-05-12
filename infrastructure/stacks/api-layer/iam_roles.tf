b306288b8cd5cb88d3b32dee88efc221a4fad21a
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-05-12 16:54:35 +0100

    decoupled api-layer from iam-developer-roles

--- infrastructure/stacks/api-layer/iam_roles.tf
+++ infrastructure/stacks/api-layer/iam_roles.tf
@@ -1,6 +1,8 @@
-module "iam_permissions_boundary" {
-  source = "../iams-developer-roles"
+
+data "aws_iam_policy" "permissions_boundary" {
+  arn = "arn:aws:iam::${local.current_account_id}:policy/${upper(var.project_name)}-PermissionsBoundary"
 }
+
 
 # Lambda trust policy
 data "aws_iam_policy_document" "lambda_assume_role" {
@@ -28,12 +30,12 @@ data "aws_iam_policy_document" "dps_assume_role" {
 resource "aws_iam_role" "eligibility_lambda_role" {
   name                 = "eligibility_lambda-role"
   assume_role_policy   = data.aws_iam_policy_document.lambda_assume_role.json
-  permissions_boundary = module.iam_permissions_boundary.permissions_boundary_arn
+  permissions_boundary = data.aws_iam_policy.permissions_boundary.arn
 }
 
 
 resource "aws_iam_role" "write_access_role" {
   name                 = "external-write-role"
   assume_role_policy   = data.aws_iam_policy_document.dps_assume_role.json
-  permissions_boundary = module.iam_permissions_boundary.permissions_boundary_arn
+  permissions_boundary = data.aws_iam_policy.permissions_boundary.arn
 }
