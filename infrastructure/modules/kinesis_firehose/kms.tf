718f98b292cc00b052b3c97b540936cb0cd6be8f
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-06-27 12:39:24 +0100

    key name change

--- infrastructure/modules/kinesis_firehose/kms.tf
+++ infrastructure/modules/kinesis_firehose/kms.tf
@@ -1,12 +1,14 @@
 resource "aws_kms_key" "firehose_cmk" {
-  description             = "KMS key for Kinesis Firehose encryption"
-  deletion_window_in_days = 7
+  description             = "${terraform.workspace == "default" ? "" : "${terraform.workspace}-"}${var.audit_firehose_delivery_stream_name} Master Key"
+  deletion_window_in_days = 14
+  is_enabled              = true
   enable_key_rotation     = true
+  tags                    = var.tags
 }
 
 
 resource "aws_kms_alias" "firehose_cmk" {
-  name          = "alias/kinesis-firehose-${var.environment}"
+  name          = "alias/${terraform.workspace == "default" ? "" : "${terraform.workspace}-"}${var.audit_firehose_delivery_stream_name}-cmk"
   target_key_id = aws_kms_key.firehose_cmk.key_id
 }
 
