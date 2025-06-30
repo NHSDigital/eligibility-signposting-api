4c540e77c6b8062ed86893ae0ef26b51eab9bd31
Adam Waller <176666249+adam-wallernhs1@users.noreply.github.com>
2025-06-30 13:21:36 +0100

    MORE TIDYING

--- tests/e2e/tests/test_smoke_tests.py
+++ tests/e2e/tests/test_smoke_tests.py
@@ -6,12 +6,11 @@ from tests.e2e.tests.test_config import *
 from tests.e2e.utils.data_loader import load_all_expected_responses, initialise_tests
 from tests.e2e.utils.s3ConfigManager import S3ConfigManager
 
-# UPDATE THE BELOW WITH LOCATION OF TEST DATA, EXPECTED RESPONSES AND CONFIGS IN DEFINED IN test_config.py #
-#############################################################################################################
+# Update the below with the configuration values specified in test_config.py
 all_data, dto = initialise_tests(SMOKE_TEST_DATA)
 all_expected_responses = load_all_expected_responses(SMOKE_TEST_RESPONSES)
 config_path = SMOKE_TEST_CONFIGS
-#############################################################################################################
+
 s3_manager = S3ConfigManager(S3_BUCKET, S3_PREFIX)
 
 
@@ -24,11 +23,11 @@ id_list = [
 @pytest.mark.smoketest
 @pytest.mark.parametrize("filename, scenario", param_list, ids=id_list)
 def test_run_smoke_case(filename, scenario, eligibility_client):
-    # get the nhs_number
+    # get the nhs_number from the scenario
     nhs_number = scenario["nhs_number"]
-    # get the associated campaign config file
+    # get the associated campaign config file from the scenario
     config_filename = scenario.get("config_filename", "")
-    # upload that config file to s3 if it
+    # upload that config file to s3 if it is missing or has changed
     s3_manager.upload_if_missing_or_changed(os.path.abspath(os.path.join(config_path, config_filename)))
 
     actual_response = eligibility_client.make_request(nhs_number, strict_ssl=False)
