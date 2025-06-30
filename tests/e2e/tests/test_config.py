3dd5368dc3f1f0f714770167d1c6375dfe1e3b42
Adam Waller <176666249+adam-wallernhs1@users.noreply.github.com>
2025-06-30 11:09:40 +0100

    moved more files

---
+++ tests/e2e/tests/test_config.py
@@ -0,0 +1,8 @@
+S3_BUCKET = "eligibility-signposting-api-test-eli-rules"
+S3_PREFIX = "manual-uploads"
+
+#############################################################################
+SMOKE_TEST_DATA = "tests/e2e/data/dynamoDB/smokeTestData/"
+SMOKE_TEST_RESPONSES = "tests/e2e/data/responses/smokeTestResponses/"
+SMOKE_TEST_CONFIGS = "tests/e2e/data/configs/smokeTestConfigs"
+#############################################################################
