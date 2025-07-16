f86605b6d45f8076e13567d4471f979aeac3f799
Shweta <216860557+shweta-nhs@users.noreply.github.com>
2025-07-16 13:58:11 +0100

    ELI-331: Fix formatting

--- src/eligibility_signposting_api/services/eligibility_services.py
+++ src/eligibility_signposting_api/services/eligibility_services.py
@@ -53,8 +53,7 @@ class EligibilityService:
                 )
 
                 logger.info("Test data masking person data: %r", person_data[0])
-                logger.info("Test data masking campaign config data: %r",
-                            campaign_configs[0].model_dump(by_alias=True))
+                logger.info("Test data masking campaign config data: %r", campaign_configs[0].model_dump(by_alias=True))
 
             except NotFoundError as e:
                 raise UnknownPersonError from e
