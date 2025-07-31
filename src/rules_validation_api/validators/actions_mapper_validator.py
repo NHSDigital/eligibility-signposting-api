050da5187f787d3f3cd93a57d2a4c8df76e684f3
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-07-31 16:55:31 +0100

    Actions mapper validator

---
+++ src/rules_validation_api/validators/actions_mapper_validator.py
@@ -0,0 +1,5 @@
+from eligibility_signposting_api.model.campaign_config import ActionsMapper
+
+
+class ActionsMapperValidator(ActionsMapper):
+    pass
--- src/rules_validation_api/validators/iteration_validator.py
+++ src/rules_validation_api/validators/iteration_validator.py
@@ -1,8 +1,11 @@
 from pydantic import Field
 
 from eligibility_signposting_api.model.campaign_config import Iteration
+from rules_validation_api.validators.actions_mapper_validator import ActionsMapperValidator
 from rules_validation_api.validators.iteration_rules_validator import IterationRuleValidation
 
 
 class IterationValidation(Iteration):
     iteration_rules: list[IterationRuleValidation] = Field(..., alias="IterationRules")
+    actions_mapper: ActionsMapperValidator = Field(..., alias="ActionsMapper")
+
