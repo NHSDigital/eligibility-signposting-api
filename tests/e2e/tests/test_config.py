12fc10f49c916571147f4fc917a19c80929e9f06
Adam Waller <176666249+adam-wallernhs1@users.noreply.github.com>
2025-08-01 13:16:44 +0100

    added vita int tests, ELI-365,

--- tests/e2e/tests/test_config.py
+++ tests/e2e/tests/test_config.py
@@ -11,9 +11,12 @@ REGRESSION_TEST_DATA: str = "data/dynamoDB/regressionTestData/"
 REGRESSION_RESPONSES: str = "data/responses/regressionTestResponses/"
 REGRESSION_CONFIGS: str = "data/configs/regressionTestConfigs"
 
-# Story Test Data Paths
+# In Progress Test Data Paths
 IN_PROGRESS_TEST_DATA: str = "data/dynamoDB/inProgressTestData/"
 IN_PROGRESS_RESPONSES: str = "data/responses/inProgressTestResponses/"
 IN_PROGRESS_CONFIGS: str = "data/configs/inProgressTestConfigs"
 
-INTEGRATION_TEST_DATA: str = "data/dynamoDB/integrationTestData/"
+# Vita Integration Test Data Paths
+VITA_INTEGRATION_TEST_DATA: str = "data/dynamoDB/vitaIntegrationTestData/"
+VITA_INTEGRATION_RESPONSES: str = "data/responses/vitaIntegrationTestResponses/"
+VITA_INTEGRATION_CONFIGS: str = "data/configs/vitaIntegrationTestConfigs"
