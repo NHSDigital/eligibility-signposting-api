eb47600032dff29d630e5c74431e6e7cc9f6b728
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-10-02 16:48:13 +0100

    infra: add firehose resource dependency for firehose cloudwatch logs

--- infrastructure/modules/kinesis_firehose/outputs.tf
+++ infrastructure/modules/kinesis_firehose/outputs.tf
@@ -3,7 +3,8 @@ output "firehose_stream_name" {
 }
 
 output "firehose_stream" {
-  value = aws_kinesis_firehose_delivery_stream.eligibility_audit_firehose_delivery_stream}
+  value = aws_kinesis_firehose_delivery_stream.eligibility_audit_firehose_delivery_stream
+}
 
 output "kinesis_firehose_cmk_arn" {
   value = aws_kms_key.firehose_cmk.arn
