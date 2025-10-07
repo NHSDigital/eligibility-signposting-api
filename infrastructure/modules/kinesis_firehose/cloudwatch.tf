ce62843aeff72b73be82b2790c03343c184be8fd
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-10-07 16:30:16 +0100

    added dependency for policy

--- infrastructure/modules/kinesis_firehose/cloudwatch.tf
+++ infrastructure/modules/kinesis_firehose/cloudwatch.tf
@@ -3,7 +3,7 @@
 resource "aws_cloudwatch_log_group" "firehose_audit" {
   name              = "/aws/kinesisfirehose/${var.project_name}-${var.environment}-audit"
   retention_in_days = 365
-  kms_key_id        =aws_kms_key.firehose_cmk.arn
+  kms_key_id        = aws_kms_key.firehose_cmk.arn
 
   tags = {
     Name  = "kinesis-firehose-logs"
@@ -11,7 +11,8 @@ resource "aws_cloudwatch_log_group" "firehose_audit" {
   }
 
   depends_on = [
-   aws_kms_key.firehose_cmk
+    aws_kms_key.firehose_cmk,
+    aws_kms_key_policy.firehose_key_policy
   ]
 }
 
