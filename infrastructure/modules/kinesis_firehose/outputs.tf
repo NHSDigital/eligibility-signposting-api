7ad139fe50fce4f07ed082adcde47c5878aba317
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-10-08 09:41:47 +0100

    infra: add kms policy dependency for firehouse cloudwatch logs

--- infrastructure/modules/kinesis_firehose/outputs.tf
+++ infrastructure/modules/kinesis_firehose/outputs.tf
@@ -5,3 +5,11 @@ output "firehose_stream_name" {
 output "kinesis_firehose_cmk_arn" {
   value = aws_kms_key.firehose_cmk.arn
 }
+
+output "kinesis_firehose_cmk" {
+  value = aws_kms_key.firehose_cmk
+}
+
+output "kinesis_firehose_key_policy" {
+  value = aws_kms_key_policy.firehose_key_policy
+}
