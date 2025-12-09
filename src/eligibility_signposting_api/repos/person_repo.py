95c2e73a879c88d724b7eea4d12d36206073f6c5
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-12-09 15:25:34 +0000

    eli-540 log corrected

--- src/eligibility_signposting_api/repos/person_repo.py
+++ src/eligibility_signposting_api/repos/person_repo.py
@@ -60,9 +60,10 @@ class PersonRepo:
         nhs_hashed_with_current = self._hashing_service.hash_with_current_secret(nhs_number)
         if nhs_hashed_with_current:
             items = self.get_person_record(nhs_hashed_with_current)
-        if not items:
-            logger.warning("The AWSCURRENT secret was tried, but no person record was found")
+            if not items:
+                logger.warning("The AWSCURRENT secret was tried, but no person record was found")
 
+        if not items:
             # Hash using AWSPREVIOUS secret and fetch items
             nhs_hashed_with_previous = self._hashing_service.hash_with_previous_secret(nhs_number)
             if nhs_hashed_with_previous:
