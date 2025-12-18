dfcff797107389466cff584be7323b5a4dbea4cb
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2025-12-18 13:20:45 +0000

    ELI-578 added dummy consumer id

--- src/eligibility_signposting_api/model/consumer_mapping.py
+++ src/eligibility_signposting_api/model/consumer_mapping.py
@@ -6,6 +6,6 @@ from eligibility_signposting_api.model.campaign_config import CampaignID
 
 ConsumerId = NewType("ConsumerId", str)
 
-class ConsumerMapping(RootModel[dict[str, list[CampaignID]]]):
-    def get(self, key: str, default: list[CampaignID] | None = None) -> list[CampaignID] | None:
+class ConsumerMapping(RootModel[dict[ConsumerId, list[CampaignID]]]):
+    def get(self, key: ConsumerId, default: list[CampaignID] | None = None) -> list[CampaignID] | None:
         return self.root.get(key, default)
