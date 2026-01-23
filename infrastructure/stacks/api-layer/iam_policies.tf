a4ee06d2f9e130c94dfe8b4734c224c2142b10de
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2026-01-23 12:00:36 +0000

    Revert "checkov suggestions"
    This reverts commit d681b5906933dd97ee1760bf63e157e9c1d6d960.

--- infrastructure/stacks/api-layer/iam_policies.tf
+++ infrastructure/stacks/api-layer/iam_policies.tf
@@ -361,13 +361,8 @@ data "aws_iam_policy_document" "s3_consumer_mapping_kms_key_policy" {
       type = "AWS"
       identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:GenerateDataKey",
-      "kms:DescribeKey",
-    ]
-    resources = [module.s3_consumer_mappings_bucket.storage_bucket_kms_key_arn]
+    actions = ["kms:*"]
+    resources = ["*"]
   }
 
   statement {
