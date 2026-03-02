e72c9c3a2f774d1a3a21f91e7e232eb2effa4e55
karthikeyannhs <174426205+Karthikeyannhs@users.noreply.github.com>
2026-03-02 14:03:23 +0000

    ELI-615 | wip

--- src/eligibility_signposting_api/services/processors/campaign_evaluator.py
+++ src/eligibility_signposting_api/services/processors/campaign_evaluator.py
@@ -1,4 +1,3 @@
-import logging
 from collections.abc import Collection, Iterator
 from itertools import groupby
 from operator import attrgetter
@@ -8,8 +7,6 @@ from wireup import service
 from eligibility_signposting_api.model import eligibility_status
 from eligibility_signposting_api.model.campaign_config import CampaignConfig
 
-logger = logging.getLogger(__name__)
-
 
 @service
 class CampaignEvaluator:
@@ -18,43 +15,22 @@ class CampaignEvaluator:
     def get_active_campaigns(self, campaign_configs: Collection[CampaignConfig]) -> list[CampaignConfig]:
         return [cc for cc in campaign_configs if cc.campaign_live]
 
-    def get_campaign_with_latest_iteration(self, active_campaigns: list[CampaignConfig]) -> CampaignConfig | None:
-        """
-        Returns the campaign with the latest active iteration date.
+    def get_latest_campaign(self, campaign_group: list[CampaignConfig]):
+        if not campaign_group:
+            return None
 
-        1. Collect all campaigns with an active iteration.
-        2. Sort by iteration date (descending).
-        3. Extract the lead campaign, throwing an error if a tie for the latest date exists.
-        """
+        latest_date = max(c.start_date for c in campaign_group)
 
-        valid_items = []
+        latest = [c for c in campaign_group if c.start_date == latest_date]
 
-        for cc in active_campaigns:
-            try:
-                valid_items.append((cc.current_iteration.iteration_date, cc))
-            except StopIteration:
-                logger.info(
-                    "Skipping campaign ID %s as no active iteration was found.",
-                    cc.id,
-                )
+        if len(latest) == 1:
+            return latest[0]
 
-        if not valid_items:
-            latest_campaign = None
-        else:
-            max_date = max(item[0] for item in valid_items)
-            cc_with_max_iteration_date: list[CampaignConfig] = [item[1] for item in valid_items if item[0] == max_date]
-            if len(cc_with_max_iteration_date) > 1:
-                err_msg = (
-                    f"Ambiguous result: '{len(cc_with_max_iteration_date)}' active iterations "
-                    f"for target {cc_with_max_iteration_date[0].target} "
-                    f"found for date '{max_date}' "
-                    f"across campaign(s) {[cc.id for cc in cc_with_max_iteration_date]}"
-                )
-                raise ValueError(err_msg)
+        if len(latest) > 1:
+            raise ValueError(
+                f"Multiple campaigns share the latest start_date: {latest_date}")  # TODO handle it in FHIR format
 
-            latest_campaign = cc_with_max_iteration_date[0]
-
-        return latest_campaign
+        return None
 
     def get_campaign_with_latest_active_iteration_per_target(
         self, campaign_configs: Collection[CampaignConfig], conditions: list[str], requested_category: str
@@ -76,10 +52,6 @@ class CampaignEvaluator:
             sorted(active_campaigns, key=attrgetter("target")),
             key=attrgetter("target"),
         ):
-            filtered_campaigns = [
-                c for c in campaign_group if filter_all_conditions or str(condition_name) in conditions
-            ]
+            campaigns = [c for c in allowed_campaigns if filter_all_conditions or str(condition_name) in conditions]
 
-            campaign = self.get_campaign_with_latest_iteration(filtered_campaigns)
-            if campaign is not None:
-                yield (condition_name, campaign)
+            yield condition_name, self.get_latest_campaign(campaigns)
