7b295e14ae4b7b78cc437d01c0d538bfc30a14dd
TOEL2 <tom.eldridge1@nhs.net>
2026-03-20 10:57:46 +0000

    [ELI-619] - formatting

--- src/eligibility_signposting_api/config/constants.py
+++ src/eligibility_signposting_api/config/constants.py
@@ -9,7 +9,6 @@ CONSUMER_MAPPING_FILE_NAME = "consumer_mapping_config.json"
 RESERVED_TEST_CONSUMER_IDS = {"test-consumer-1", "test-consumer-2", "test-consumer-3"}
 
 TTL = {
-    "local": 0,
     "test": 300,
     "dev": 300,
     "preprod": 300,
